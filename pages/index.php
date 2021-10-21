fdd1eb55f5c0e0e1433c964fc3f23e708ffa3b51
aspolchi <aspolchi@gmail.com>
2021-10-21 17:46:08 -0300

    Update index.php

--- pages/index.php
+++ pages/index.php
@@ -1,13 +1,13 @@
 <?php
-    $nombre = $ _POST['nombre'];
-$email = $ _POST['email'];
-$mensaje = $ _POST['mensaje'];
+    $nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$mensaje = $_POST['mensaje'];
 $para = 'aspolchi@gmail.com';
 $titulo = 'Hola - La Webera';
  
 $msjCorreo = "Nombre: $nombre\n E-Mail: $email\n Mensaje:\n $mensaje";
  
-if ($ _POST['submit']) {
+if ($_POST['submit']) {
 if (mail ($para, $titulo, $msjCorreo)) {
 echo 'El mensaje se ha enviado';
 } else {
